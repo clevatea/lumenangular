9a54cacfcd28759632febb58e4965fb0070682fd
khasin <khasin.madappe@gmail.com>
2016-08-10 12:44:03 +0700

    First Commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ecentre</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	{{-- JQUERY AND BOOTSTRAP --}}
+	<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+	{{-- <meta name="csrf-token" content="{{ csrf_token() }}" /> --}}
+
+	{{-- ANGULAR MODULE --}}
+	<script src="/bower_components/angular/angular.min.js"></script>
+	<script src="/bower_components/angular-filter/dist/angular-filter.min.js"></script>
+	<script src="/bower_components/angular-animate/angular-animate.min.js"></script>
+	<script src="/bower_components/angular-sanitize/angular-sanitize.min.js"></script>
+	<script src="/bower_components/angular-cookies/angular-cookies.min.js"></script>
+	<script src="/bower_components/angular-ui-validate/dist/validate.min.js"></script>
+	<script src="/bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+	<script src="/bower_components/angular-strap/dist/angular-strap.js"></script>
+	<script src="/bower_components/angular-strap/dist/angular-strap.tpl.js"></script>
+	<script src="/bower_components/satellizer/dist/satellizer.min.js"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+
+</head>
+<body>
+	
+</body>
+</html>
